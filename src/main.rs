c3f384acfac2e09148383de818ff90c93dfc6224
Jakub Kolodziej <jakvb.kolodziej@gmail.com>
2023-03-25 13:14:06 +0100

    first exercise in set 3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::{io};
 mod set1;
 mod set2;
+mod set3;
 mod basic;
 
 fn main() 
@@ -12,6 +13,7 @@ fn main()
     {
         1 => set1::start(),
         2 => set2::start(),
+        3 => set3::start(),
         _ => println!("Wrong number")
     
     };
--- src/set3.rs
+++ src/set3.rs
@@ -7,18 +7,28 @@ use std::{io, cmp::Ordering};
 pub fn start() {
     println!("Which exercise to open?");
     let x = input_int();
-    // match x
-    // {
-    //     1 => z1(),
-    //     2 => z2(),
-    //     3 => z3(),
-    //     4 => z4(),
-    //     _ => println!("Wrong number")
+    match x
+    {
+        1 => z1(),
+        // 2 => z2(),
+        // 3 => z3(),
+        // 4 => z4(),
+        _ => println!("Wrong number")
     
-    // };
+    };
 }
 
+fn z1()
+{
+    let arr = [4.2, 5.7, 3.3, 7.7, -1.0, 1.8, 6.6, 1.5, 2.2, 7.9, -6.1, 4.3, 6.3, 3.3,
+    3.2, 7.0, 2.9, 4.2, 4.8, 4.7];
+    let mut total = 0.0;
+    for i in 0..arr.len() {
+        total += arr[i];
+    }
+    println!("Avereage temperature is: {:.1} *C", total/(arr.len() as f64));
 
+}
 
 
 
